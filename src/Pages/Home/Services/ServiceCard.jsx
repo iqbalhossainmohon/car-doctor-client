336963fc46b32a86713f33e81495dd368de96d83
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-05-11 09:59:55 +0600

    check out form added

--- src/Pages/Home/Services/ServiceCard.jsx
+++ src/Pages/Home/Services/ServiceCard.jsx
@@ -1,7 +1,8 @@
+import { Link } from "react-router-dom";
 
 const ServiceCard = ({ service }) => {
 
-    const { title, img, price } = service;
+    const { _id, title, img, price } = service;
     return (
         <div className="card card-compact w-80 bg-base-100 shadow-2xl">
             <figure><img className="h-56" src={img} alt="" /></figure>
@@ -9,12 +10,12 @@ const ServiceCard = ({ service }) => {
                 <h2 className="card-title text-2xl font-bold">{title}</h2>
                 <p className="text-xl text-orange-500">Price: ${price}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-                        </svg>
-
-                    </button>
+                    
+                    <Link to={`/checkout/${_id}`}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                            </svg>
+                    </Link>
                 </div>
             </div>
         </div>
